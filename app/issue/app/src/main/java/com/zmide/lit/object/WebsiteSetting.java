cae53063c958093fe60c7ddb510af1210496f75a
ThankRain <36541432+ThankRain@users.noreply.github.com>
2020-07-25 07:50:38 +0800

    update WebsiteSetting.java
    Former-commit-id: 15ca8c4c2a9a1ba4a4e02ff9f020d676158ec2a1
    Former-commit-id: 036dacc2c767b6dfded960f434e6e9ce12d768ca

---
+++ app/issue/app/src/main/java/com/zmide/lit/object/WebsiteSetting.java
@@ -0,0 +1,62 @@
+package com.zmide.lit.object;
+
+public class WebsiteSetting {
+	public int id = 0;
+	public boolean ad_host;
+	public boolean app;
+	public boolean js;
+	public boolean no_history;
+	public boolean no_picture;
+	public String site;
+	public boolean state;
+	public int ua;
+	public setting get(int p){
+		switch (p){
+			case 0:
+				return new setting("独立网站设置",true,"",state);
+			case 1:
+			  return new setting("User Agent",false,ua,false);
+			case 2:
+				return new setting("广告拦截",true,"",ad_host);
+			case 3:
+				return new setting("打开外部应用",true,"",app);
+			case 4:
+				return new setting("JavaScript脚本",true,"",js);
+			case 5:
+				return new setting("无痕浏览",true,"",no_history);
+			case 6:
+				return new setting("无图模式",true,"",no_picture);
+		}
+		return null;
+	}
+	public WebsiteSetting set(int p,boolean t){
+		switch (p){
+			case 0:
+				state = t;
+			case 1:
+				ad_host = t;
+			case 2:
+				app = t;
+			case 3:
+				js = t;
+			case 4:
+				no_history = t;
+			case 5:
+				no_picture = t;
+		}
+		return this;
+	}
+	public class setting{
+		public String name;
+		public boolean isSwitch;
+		public String text;
+		public boolean state;
+		
+		public setting(String name, boolean isSwitch, String text, boolean state) {
+			this.name = name;
+			this.isSwitch = isSwitch;
+			this.text = text;
+			this.state = state;
+		}
+	}
+}
