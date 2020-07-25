29642108113b20ce50a4bc2732226efa6b6c1f40
ThankRain <36541432+ThankRain@users.noreply.github.com>
2020-07-26 00:17:57 +0800

    Update WebsiteSetting.java
    Former-commit-id: 5306d601dd526c18462b7a444f90f53977483f97
    Former-commit-id: 77ec5be9242b85e704fca6187dd6602df39f369f

--- app/src/main/java/com/zmide/lit/object/WebsiteSetting.java
+++ app/src/main/java/com/zmide/lit/object/WebsiteSetting.java
@@ -42,7 +42,94 @@ public class WebsiteSetting {
 				no_history = t;
 			case 5:
 				no_picture = t;
-		}package com.zmide.lit.object;
+		}package com.zmide.lit.object;package com.zmide.lit.object;
+
+public class WebsiteSetting {
+	public int id = 0;
+	public boolean ad_host;
+	public boolean app;
+	public boolean js;
+	public boolean no_history;
+	public boolean no_picture;
+	public String site;
+	public boolean state;
+	public int ua;
+	public setting get(int p){
+		switch (p){
+			case 0:
+				return new setting("独立网站设置",true,"",state);
+			case 1:
+			  return new setting("User Agent",false,ua,false);
+			case 2:
+				return new setting("广告拦截",true,"",ad_host);
+			case 3:
+				return new setting("打开外部应用",true,"",app);
+			case 4:
+				return new setting("JavaScript脚本",true,"",js);
+			case 5:
+				return new setting("无痕浏览",true,"",no_history);
+			case 6:
+				return new setting("无图模式",true,"",no_picture);
+		}
+		return null;
+	}
+	public WebsiteSetting set(int p,boolean t){
+		switch (p){
+			case 0:
+				state = t;
+				break;
+			case 2:
+				ad_host = t;
+				break;
+			case 3:
+				app = t;
+				break;
+			case 4:
+				js = t;
+				break;
+			case 5:
+				no_history = t;
+				break;
+			case 6:
+				no_picture = t;
+				break;
+		}
+		return this;
+	}
+	
+	public String text(){
+	  return "id:"+websiteSetting.id+"\n"+
+					 "site:"+site+"\n"+
+					 "js:"+js+"\n"+
+					 "ua:"+ua+"\n"+
+					 "app:"+app+"\n"+
+				   "state:"+state+"\n"+
+					 "no_history:"+no_history+"\n"+
+					 "no_picture:"+no_picture;
+	}
+	public WebsiteSetting set(int p,int t){
+		switch (p){
+			case 1:
+				ua = t;
+				break;
+		}
+		return this;
+	}
+	public class setting{
+		public String name;
+		public boolean isSwitch;
+		public String text;
+		public boolean state;
+		
+		public setting(String name, boolean isSwitch, String text, boolean state) {
+			this.name = name;
+			this.isSwitch = isSwitch;
+			this.text = text;
+			this.state = state;
+		}
+	}
+}
+
 
 public class WebsiteSetting {
 	public int id = 0;
