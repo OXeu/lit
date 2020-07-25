360ae469ab85831977227f172c3793dcac04f3a1
ThankRain <36541432+ThankRain@users.noreply.github.com>
2020-07-25 23:22:06 +0800

    update WebsiteUtils.java
    Former-commit-id: d41416c4f25d2955c8136590a295a0801ae8ba0b
    Former-commit-id: f5199267324fa3cfa0278585c1698fef3ee4cbe1

--- app/src/main/java/com/zmide/lit/util/WebsiteUtils.java
+++ app/src/main/java/com/zmide/lit/util/WebsiteUtils.java
@@ -3,7 +3,7 @@ package com.zmide.lit.util;
 import android.content.Context;
 
 import com.zmide.lit.object.WebsiteSetting;
-
+import com.zmide.lit.util.Chiper;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
@@ -38,6 +38,7 @@ public class WebsiteUtils {
 	}
 	
 	public static void putWebsiteSetting(Context context , WebsiteSetting websiteSetting) {
+	  Chiper.copy(websiteSetting.toString());
 		if (websiteSetting.id != 0) {
 			WebsiteSetting websiteSetting1 = DBC.getInstance(context).getWebsiteSetting(websiteSetting.id);
 			if (websiteSetting1 != null) {//已存在，修改
