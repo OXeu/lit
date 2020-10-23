70266a5615b83f1d7d8e2a01487649f12f939925
ThankRain <1573856599@qq.com>
2020-10-24 01:09:21 +0800

    fix
    Former-commit-id: f80befa89b2e3173a447c0d9d782c69985722f39
    Former-commit-id: 37f64c3ed5faad4346d7a2a3fee082247e933930

--- app/src/main/java/com/zmide/lit/helper/CtrlableLinearLayoutManager.java
+++ app/src/main/java/com/zmide/lit/helper/CtrlableLinearLayoutManager.java
@@ -9,7 +9,7 @@ import android.util.Log;
 public class CtrlableLinearLayoutManager extends LinearLayoutManager {
     private boolean mCanVerticalScroll = true;
 
-	private boolean mCanHorizontalScroll = true;;
+	private boolean mCanHorizontalScroll = false;;
 
     public CtrlableLinearLayoutManager(Context context,int direction,boolean b) {
         super(context,direction,b);
