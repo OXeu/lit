db95e46d8d1988bddcf08689ea040b4ae897895b
ThankRain <1573856599@qq.com>
2020-10-26 00:05:06 +0800

    更换RecyclerView
    Former-commit-id: ec900b6c659a738bdfbfd106cda7f4dd1f3b852e
    Former-commit-id: 4b81b2c971ecc2c6cfb24d6790a5a284da64e6fe

---
+++ app/src/main/java/com/zmide/lit/view/UnTouchableRecyclerView.java
@@ -0,0 +1,45 @@
+package com.zmide.lit.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zmide.lit.R;
+import android.view.MotionEvent;
+
+/**
+ * Copyright (C), 2019-2020, DreamStudio
+ * Author: Xeu
+ * Date: 2020/2/27 16:27
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+public class UnTouchableRecyclerView extends RecyclerView {
+	private int mMaxHeight;
+
+	public UnTouchableRecyclerView(Context context) {
+		super(context);
+	}
+
+	public UnTouchableRecyclerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+
+	public UnTouchableRecyclerView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		return false;
+	}
+
+	
+	
+}
