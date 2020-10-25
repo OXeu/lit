a1761ad68b4ed06a66f0717cfc98eb5750a0ac2f
ThankRain <1573856599@qq.com>
2020-10-26 00:17:31 +0800

    最后一次挣扎
    Former-commit-id: de207cfb31e7f2946002fbd4989268b644415114
    Former-commit-id: bbfecd4ef0d621bf9d60ab88376b8cd2096dc973

--- app/src/main/java/com/zmide/lit/main/MWeb.java
+++ app/src/main/java/com/zmide/lit/main/MWeb.java
@@ -185,17 +185,18 @@ public class MWeb {
 					Log.i("action",event.getAction()+"");
 					switch (event.getAction()){
 						case MotionEvent.ACTION_DOWN://如果是按下的话
-					MToastUtils.makeText("按下").show();						WebContainerPlus.getLayoutManager().setCanHorizontalScroll(true);//设置Recyclerview无法滚动
+					MToastUtils.makeText("按下").show();		
 					break;
 					case MotionEvent.ACTION_MOVE:
 						rv.onTouchEvent(event);
 						break;
-						
+
 					case MotionEvent.ACTION_UP ://如果是抬起的话
 						MToastUtils.makeText("抬起").show();
 						WebContainerPlus.getLayoutManager().setCanHorizontalScroll(false);//设置Recyclerview可以滚动
 						break;
 					}
+					rv.onTouchEvent(event);
 					return true;
 				}
 				
