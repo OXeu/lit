52b60d3c222410a2f46bbdaa6fc1e59b5b8322db
ThankRain <1573856599@qq.com>
2020-10-27 01:29:05 +0800

    test
    Former-commit-id: 9e82f788f39497acde787d2dd0620c0f87f03851
    Former-commit-id: b34636e4e07e2aa7cc15f6e717b64095f6f8ce28

--- app/src/main/java/com/zmide/lit/windows/FloatMemoryObserver.java
+++ app/src/main/java/com/zmide/lit/windows/FloatMemoryObserver.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.FrameLayout;
+import android.os.Debug;
+import android.content.Context;
 public class FloatMemoryObserver {
 	private View floatView;
     private Handler handler = new Handler();
@@ -78,6 +80,77 @@ public class FloatMemoryObserver {
         text += "MBytes\nmaxMemory: "+maxMemory;
         text += "MBtyes\ntotalMemory: "+totalMemory;
         text += "MBytes\nfreeMemory: "+freeMemory+"MBytes";
+		text += "\nMemory:"+ getRunningMemory() + "MBytes";
 		return text;
 	}
+	
+	private double getRunningMemory() {
+        double mem = 0.0D;
+        try {
+            // 统计进程的内存信息 totalPss
+			ActivityManager mActivityManager = (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
+            final Debug.MemoryInfo[] memInfo = mActivityManager.getProcessMemoryInfo(new int[]{android.os.Process.myPid()});
+            if (memInfo.length > 0) {
+
+                /**
+                 * 读取内存信息,跟Android Profiler 分析一致
+                 */
+                String java_mem = memInfo[0].getMemoryStat("summary.java-heap");
+
+                String native_mem = memInfo[0].getMemoryStat("summary.native-heap");
+
+                String graphics_mem = memInfo[0].getMemoryStat("summary.graphics");
+
+                String stack_mem = memInfo[0].getMemoryStat("summary.stack");
+
+                String code_mem = memInfo[0].getMemoryStat("summary.code");
+
+                String others_mem = memInfo[0].getMemoryStat("summary.system");
+
+                final int dalvikPss = convertToInt(java_mem,0)
+					+ convertToInt(native_mem,0)
+					+ convertToInt(graphics_mem,0)
+					+ convertToInt(stack_mem,0)
+					+ convertToInt(code_mem,0)
+					+ convertToInt(others_mem,0);
+
+                if (dalvikPss >= 0) {
+                    // Mem in MB
+                    mem = dalvikPss / 1024.0D;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mem;
+    }
+
+    /**
+     * 转化为int
+     * @param value 传入对象
+     * @param defaultValue 发生异常时，返回默认值
+     * @return
+     */
+    public final static int convertToInt(Object value, int defaultValue){
+
+        if (value == null || "".equals(value.toString().trim())){
+            return defaultValue;
+        }
+
+        try {
+            return Integer.valueOf(value.toString());
+        }catch (Exception e){
+
+            try {
+                return Integer.valueOf(String.valueOf(value));
+            }catch (Exception e1) {
+
+                try {
+                    return Double.valueOf(value.toString()).intValue();
+                }catch (Exception e2){
+                    return defaultValue;
+                }
+            }
+        }
+    }
 }
